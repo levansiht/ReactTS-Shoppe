e5b131cce19ff9064826cfe1892e4ce445c1d80a
levansiht <levansia1ct@gmail.com>
2024-10-15 11:21:06 +0700

    Feat: UI login register

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import useRouteElements from "./useRouteElements"
+
+
+function App() {
+  const  routeElements = useRouteElements()
+  return (
+    <div>{routeElements}</div>
+  )
+}
+
+export default App
